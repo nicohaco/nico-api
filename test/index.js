8a23dcddc2e04185a5e38e7389102ba21bf1f516
Yuta Hiroto <git@about-hiroppy.com>
2017-05-18 17:43:23 +0900

    feat(video): add search api

--- test/index.js
+++ test/index.js
@@ -22,5 +22,15 @@ test('test flow', async (t) => {
   const res = await n.video.getFLV(items[0].item_data.video_id);
   const nicohistory = await n.video.getNicohistory(items[0].item_data.video_id);
 
+  // http://api.search.nicovideo.jp/api/v2/video/contents/search?q=%E5%88%9D%E9%9F%B3%E3%83%9F%E3%82%AF&targets=title&fields=contentId,title,description,tags,categoryTags,viewCounter,mylistCounter,commentCounter,startTime,thumbnailUrl&filters%5BviewCounter%5D%5Bgte%5D=1000000&_sort=-viewCounter&_offset=0&_limit=100&_context=apiguide
+  const search = await n.video.search({
+    q: '初音ミク',
+    targets : 'title',
+    _sort   : '-viewCounter',
+    _context: 'nicoapi',
+    _limit: 100,
+    fields: 'contentId,title,description,tags,categoryTags,viewCounter,mylistCounter,commentCounter,startTime,thumbnailUrl'
+  });
+
   t.pass();
 });
