de8fe494d1d4bf53390d04acdcbe6b8602f124bd
Yuta Hiroto <git@about-hiroppy.com>
2017-05-21 22:52:54 +0900

    feat(mylist): add addMylist and delteMylist

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+/* eslint-disable camelcase */
 
 import test from 'ava';
 import dotenv from 'dotenv';
@@ -14,26 +15,38 @@ test('test flow', async (t) => {
 
   await n.login();
 
-  const list = await n.mylist.getAll();
+  // const list = await n.mylist.getAll();
+  //
+  // // 新しいマイリスト(1)を取得
+  // const id = list.mylistgroup.find((item) => item.name === '新しいマイリスト(1)').id;
+  // const items = (await n.mylist.get(id)).mylistitem;
+  // console.log(items)
+  // const res = await n.video.getFLV(items[0].item_data.video_id);
+  // const nicohistory = await n.video.getNicohistory(items[0].item_data.video_id);
+  //
+  // const search = await n.video.search({
+  //   q       : '初音ミク',
+  //   targets : 'title',
+  //   _sort   : '-viewCounter',
+  //   _context: 'nicoapi',
+  //   _limit  : 100,
+  //   fields  : 'contentId,title,description,tags,categoryTags,viewCounter,mylistCounter,commentCounter,startTime,thumbnailUrl'
+  // });
+  //
+  // const userInfo = await n.user.getInfo();
 
-  // 新しいマイリスト(1)を取得
-  const id = list.mylistgroup.find((item) => item.name === '新しいマイリスト(1)').id;
-  const items = (await n.mylist.get(id)).mylistitem;
-  const res = await n.video.getFLV(items[0].item_data.video_id);
-  const nicohistory = await n.video.getNicohistory(items[0].item_data.video_id);
+  // const newMylistId = await n.mylist.create({
+  //   name: '私の',
+  //   description: '',
+  //   public: 0,
+  //   default_sort: 0,
+  //   icon_id: 0
+  // });
 
-  // http://api.search.nicovideo.jp/api/v2/video/contents/search?q=%E5%88%9D%E9%9F%B3%E3%83%9F%E3%82%AF&targets=title&fields=contentId,title,description,tags,categoryTags,viewCounter,mylistCounter,commentCounter,startTime,thumbnailUrl&filters%5BviewCounter%5D%5Bgte%5D=1000000&_sort=-viewCounter&_offset=0&_limit=100&_context=apiguide
-  const search = await n.video.search({
-    q       : '初音ミク',
-    targets : 'title',
-    _sort   : '-viewCounter',
-    _context: 'nicoapi',
-    _limit  : 100,
-    fields  : 'contentId,title,description,tags,categoryTags,viewCounter,mylistCounter,commentCounter,startTime,thumbnailUrl'
+  const res = await n.mylist.deleteVideo({
+    group_id      : 59065359,
+    'id_list[0][]': 9 // 動画IDではなくitem_id
   });
 
-  const userInfo = await n.user.getInfo();
-
-  console.log(userInfo);
   t.pass();
 });
